ad8f059ef38cbf9dd27d2dd656409567a0ccce1b
Dipanshu Somra <56351944+DipanshuSomra@users.noreply.github.com>
2022-06-16 11:59:36 +0530

    Update Day3 Parcal's triangle.cpp

--- Day3 Parcal's triangle.cpp
+++ Day3 Parcal's triangle.cpp
@@ -1,3 +1,12 @@
+/*For example, given integer N= 4 then you have to print.
+1  
+1 1 
+1 2 1 
+1 3 3 1
+
+Here for the third row, you will see that the second element is the summation
+of the above two-row elements i.e. 2=1+1, and similarly for row three 3 = 1+2 and 3 = 1+2.*/
+
 #include <bits/stdc++.h> 
 vector<vector<long long int>> printPascal(int n) 
 {
@@ -6,9 +15,8 @@ vector<vector<long long int>> printPascal(int n)
     
     for(int i = 0; i < n; i++){
         
-        //initialize 1st and 2nd position with 1
-        pascalTree[i].resize(i+1);
-        pascalTree[i][0] = pascalTree[i][i] = 1; 
+        pascalTree[i].resize(i+1); // change the size of column to i+1 so that it will become a tree
+        pascalTree[i][0] = pascalTree[i][i] = 1; //initialize 1st and 2nd position with 1
         
         for(int j = 1; j < i; j++){
             
