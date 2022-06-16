99401276ea32d500fa8dead7ba8d59c0933c7248
Dipanshu Somra <56351944+DipanshuSomra@users.noreply.github.com>
2022-06-16 11:53:24 +0530

    Update Day2 Set matrix zeros.cpp

--- Day2 Set matrix zeros.cpp
+++ Day2 Set matrix zeros.cpp
@@ -1,3 +1,15 @@
+/*Problem Statement
+Given an ‘N’ x ‘M’ integer matrix, if an element is 0, set its entire row and column to 0's, and return the matrix. In particular, your task is to modify it in such a way that if a cell has a value 0 (matrix[i][j] == 0), then all the cells of the ith row and jth column should be changed to 0.
+You must do it in place.
+For Example:
+If the given grid is this:
+[7, 19, 3]
+[4, 21, 0]
+
+Then the modified grid will be:
+[7, 19, 0]
+[0, 0,  0]*/
+
 #include <bits/stdc++.h> 
 void setZeros(vector<vector<int>> &matrix)
 {
@@ -5,7 +17,7 @@ void setZeros(vector<vector<int>> &matrix)
     int rows = matrix.size();
     int cols = matrix[0].size();
     
-    //2. taking variable to store the original value of matrix[0][0]
+    //2. taking variable to store the original value at matrix[0][0]
     int col0 = 1;
     
     for(int i = 0; i < rows; i++){
