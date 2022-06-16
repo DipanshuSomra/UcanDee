45e625360ef1652187415f05a959012f8cd0d568
Dipanshu Somra <56351944+DipanshuSomra@users.noreply.github.com>
2022-06-16 11:31:43 +0530

    Update Day1. Sort012.cpp

--- Day1. Sort012.cpp
+++ Day1. Sort012.cpp
@@ -2,20 +2,20 @@
 void sort012(int *arr, int n)
 {
    //   Write your code here
-   int low = 0;
-   int mid = 0;
-   int high = n-1;
+   int low = 0;  // for storing 0's
+   int mid = 0;  // for storing 1's
+   int high = n-1;  // for storing 2's
 
    while(mid <=high){
        if(arr[mid] == 0){
-           swap(arr[low],arr[mid]);
+           swap(arr[low],arr[mid]);  // if find 0 at swap low and mid and increment both
            low++;
            mid++;
        }
-       else if(arr[mid] == 1){
+       else if(arr[mid] == 1){  // if find 1 increment mid
            mid++;
        }
-       else if(arr[mid] == 2){
+       else if(arr[mid] == 2){   // if find 2 at swap high and mid and decrement high
            swap(arr[mid], arr[high]);
            high--;
        }
